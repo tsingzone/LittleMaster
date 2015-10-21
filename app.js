71d8446c92a102def1c77a108f475d4dfbfce994
MichelFeng <277241073.com>
2015-10-21 19:38:06 +0800

    check render view path

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ var admin = require('./routes/admin/admin');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + "/views"));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,10 +27,10 @@ app.use('/admin', admin); // admin后台
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -38,23 +38,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/weixin/teacher/teacher.js
+++ routes/weixin/teacher/teacher.js
@@ -3,15 +3,22 @@
  */
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+// 获取视图路径
+var getView = function(view){
+    var dir = "weixin/teacher/";
+    return path.join(dir, view);
+};
 
 router.get('/', function (req, res) {
-    console.log("teacher.js /")
-    res.render('teacher', {title: "Teacher"});
+    console.log("teacher.js /");
+    res.render(getView("teacher"), {title: "Teacher"});
 });
 
 router.get('/get', function (req, res) {
     console.log("teacher.js get");
-    res.render('teacher', {title: "get"});
+    res.render(getView("teacher"), {title: "get"});
 });
 
 module.exports = router;
