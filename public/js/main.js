4164be674680952c7979f175a4a3a0d5aa6160e7
Maskerr <gaoxinpengai180@126.com>
2015-10-22 12:29:39 +0800

    模板样式设计

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by GXP on 2015/10/22.
+ */
+$(document).on('click','#find_job',function() {
+    window.location.href = '/weixin/teacher';
+});
+$(document).on('click','#personal_center',function() {
+    window.location.href = '/weixin/company';
+});
