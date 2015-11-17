d7c7aa5925b99dd676beb36fb8810509a9211fd2
MichelFeng <277241073.com>
2015-11-17 18:52:36 +0800

    sign jobs and collect jobs

--- models/weixin/teacher/JobModel.js
+++ models/weixin/teacher/JobModel.js
@@ -39,27 +39,24 @@ var JobModel = {
          */
         jobModel.getCollectJobs = function (source, callback) {
             var sql = 'select '
-                + ' teacher_sign.id as id,'
-                + ' teacher_sign.teacher_id as teacherId, '
-                + ' company_job.title as title,'
-                + ' sys_position.`name` as position,'
-                + ' company_job.start_time as startTime,'
-                + ' company_job.end_time as endTime,'
-                + ' company_job.gender as gender,'
-                + ' company_job.salary as salary,'
-                + ' sys_salary_type.`name` as salaryType,'
-                + ' sys_settlement.name as settlement,'
-                + ' company_job.address'
-                + ' FROM teacher_sign'
-                + ' left join company_job'
-                + ' on teacher_sign.job_id = company_job.id'
-                + ' left join sys_position'
-                + ' on sys_position.id = company_job.position_id'
-                + ' left join sys_salary_type'
-                + ' on sys_salary_type.id = company_job.salary_type'
-                + ' left join sys_settlement'
-                + ' on sys_settlement.id = company_job.settlement_id'
-                + ' where teacher_sign.teacher_id = ?';
+                + ' company_job.id as jobId, '
+                + ' company_job.title as title, '
+                + ' sys_position.`name` as position, '
+                + ' company_job.start_time as startTime, '
+                + ' company_job.end_time as endTime, '
+                + ' company_job.gender as gender, '
+                + ' company_job.salary as salary, '
+                + ' sys_salary_type.`name` as salaryType, '
+                + ' sys_settlement.name as settlement, '
+                + ' company_job.address '
+                + ' from teacher_collection '
+                + ' left join company_job on company_job.id = teacher_collection.job_id '
+                + ' left join sys_position on sys_position.id = company_job.position_id '
+                + ' left join sys_salary_type on sys_salary_type.id = company_job.salary_type '
+                + ' left join sys_settlement on sys_settlement.id = company_job.settlement_id '
+                + ' where company_job.`status` = 1 '
+                + ' and teacher_collection.`status` = 1'
+                + ' and teacher_collection.teacher_id = ?';
             jobModel.queryDb({
                 sql: sql,
                 params: [source.teacherId]
@@ -85,7 +82,7 @@ var JobModel = {
                 + ' where teacher_sign.teacher_id = ? '
                 + ' and company_job.`status` <> -1 '
                 + ' and teacher_sign.`status` <> -1 '
-                + ' and teacher_sign.progress <> 0 ';
+                + ' and teacher_sign.progress > 0 ';
             jobModel.queryDb({
                 sql: sql,
                 params: [teacherId]
@@ -98,21 +95,16 @@ var JobModel = {
          * @param callback
          */
         jobModel.getCollectJobsCount = function (teacherId, callback) {
-            var sql = 'select  ifnull(count(teacher_sign.id), 0) as collectCount '
-                + ' FROM teacher_sign '
-                + ' left join company_job '
-                + ' on teacher_sign.job_id = company_job.id '
-                + ' left join sys_position '
-                + ' on sys_position.id = company_job.position_id '
-                + ' left join sys_salary_type '
-                + ' on sys_salary_type.id = company_job.salary_type '
-                + ' left join sys_settlement '
-                + ' on sys_settlement.id = company_job.settlement_id '
-                + ' where teacher_sign.teacher_id = ? '
-                + ' and company_job.`status` <> -1 '
-                + ' and teacher_sign.`status` <> -1 '
-                + ' and teacher_sign.progress = 0 '
-                + ' and teacher_sign.collection = 1 ';
+            var sql = 'select  ifnull(count(teacher_collection.id), 0) as collectCount '
+                + ' from teacher_collection '
+                + ' left join company_job on company_job.id = teacher_collection.job_id '
+                + ' left join sys_position on sys_position.id = company_job.position_id '
+                + ' left join sys_salary_type on sys_salary_type.id = company_job.salary_type '
+                + ' left join sys_settlement on sys_settlement.id = company_job.settlement_id '
+                + ' where company_job.`status` = 1 '
+                + ' and teacher_collection.`status` = 1'
+                + ' and teacher_collection.teacher_id = ?'
+                + ' ';
             jobModel.queryDb({
                 sql: sql,
                 params: [teacherId]
